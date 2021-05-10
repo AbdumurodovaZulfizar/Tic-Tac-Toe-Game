bbcb8555e6c368655712b9cef5c5d270c7ef9dbd
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-05-10 22:38:31 +0500

    Craete conditional

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 square = "+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+"
-puts "Welcome to Tic Tac Toe Game. Let's begin"
+puts "Welcome to Tic Tac Toe Game. Let's begin" + "\n"
 puts 'Enter the name of first player.'
 first_player = gets.chomp
 puts 'Enter the name of second player.'
@@ -13,12 +13,29 @@ puts "Let's start! Press Enter"
 gets
 system "clear"
 find_winner = false
-until  find_winner
-  puts square + "\n"
-  puts "It's #{name_1}'s turn to play."
-  puts 'Select one available cell from the board.'
-  number = gets.chomp
-  puts "It's #{name_2}'s turn to play."
-  puts 'Select one available cell from the board.'
-  number = gets.chomp
+arr = [1,2,3,4,5,6,7,8,9]
+3.times do
+    puts square + "\n"
+    puts "It's #{name_1}'s turn to play."
+    puts 'Select one available cell from the board.'
+    number = gets.chomp
+    ele = number.to_i
+    if arr.include?(ele)
+      system "clear"
+      puts square + "\n"
+      puts "It's #{name_2}'s turn to play."
+      puts 'Select one available cell from the board.'
+      number = gets.chomp
+        ele = number.to_i
+        if arr.include?(ele)
+          system "clear"
+        else
+          puts "Invalid value. Please enter a number between 1-9"
+          number = gets.chomp
+        end
+    else
+      puts "Invalid value. Please enter a number between 1-9"
+      number = gets.chomp
+    end
 end
+
