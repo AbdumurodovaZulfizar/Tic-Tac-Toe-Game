76f11fef828d181eeac280b0c0a6106b6469567f
Ángel Díaz <diazangel9311@gmail.com>
2021-05-10 16:09:14 -0500

    Fix rubocop issues

--- bin/main.rb
+++ bin/main.rb
@@ -1,81 +1,80 @@
 #!/usr/bin/env ruby
 square = "+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+"
-puts "Welcome to Tic Tac Toe Game. Let's begin" + "\n"
+puts "Welcome to Tic Tac Toe Game. Let's begin\n"
 puts 'Enter the name of first player.'
 first_player = gets.chomp
 puts 'Enter the name of second player.'
 second_player = gets.chomp
-name_1 = first_player[0].upcase + first_player[1..-1].downcase
-name_2 = second_player[0].upcase + second_player[1..-1].downcase
-system "clear"
-puts "#{name_1} will play with X, and #{name_2} will play with O"
+name1 = first_player[0].upcase + first_player[1..-1].downcase
+name2 = second_player[0].upcase + second_player[1..-1].downcase
+system 'clear'
+puts "#{name1} will play with X, and #{name2} will play with O"
 puts "Let's start! Press Enter"
 gets
-system "clear"
+system 'clear'
 2.times do
-  system "clear"
+  system 'clear'
   puts square
-  puts "it's #{name_1}'s turn to play."
-  puts "Please select an available cell from the board!"
+  puts "it's #{name1}'s turn to play."
+  puts 'Please select an available cell from the board!'
   number = gets.chomp
   ele = number.to_i
   until (1..9).any?(ele)
-    puts "Invalid move. Please enter a number between 1-9" unless (1..9).any?(ele)
+    puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele)
     number = gets.chomp
     ele = number.to_i
   end
-  system "clear"
+  system 'clear'
   puts square
-  puts "it's #{name_2}'s turn to play."
-  puts "Please select an available cell from the board!"
+  puts "it's #{name2}'s turn to play."
+  puts 'Please select an available cell from the board!'
   number = gets.chomp
   ele = number.to_i
   until (1..9).any?(ele)
-    puts "Invalid move. Please enter a number between 1-9" unless (1..9).any?(ele)
+    puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele)
     number = gets.chomp
     ele = number.to_i
-  end 
+  end
 end
-puts "#{name_2} you WON the game!!!"
+puts "#{name2} you WON the game!!!"
 gets
-system "clear"
-arr = []
+system 'clear'
 2.times do
-  system "clear"
+  system 'clear'
   puts square
-  puts "it's #{name_1}'s turn to play."
-  puts "Please select an available cell from the board!"
+  puts "it's #{name1}'s turn to play."
+  puts 'Please select an available cell from the board!'
   number = gets.chomp
   ele1 = number.to_i
   until (1..9).any?(ele1)
-    puts "Invalid move. Please enter a number between 1-9" unless (1..9).any?(ele1)
+    puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele1)
     number = gets.chomp
     ele1 = number.to_i
   end
-  
-  system "clear"
+
+  system 'clear'
   puts square
-  puts "it's #{name_2}'s turn to play."
-  puts "Please select an available cell from the board!"
+  puts "it's #{name2}'s turn to play."
+  puts 'Please select an available cell from the board!'
   number = gets.chomp
   ele2 = number.to_i
   until (1..9).any?(ele2)
-    puts "Invalid move. Please enter a number between 1-9" unless (1..9).any?(ele2)
+    puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele2)
     number = gets.chomp
     ele2 = number.to_i
   end
 end
 
-  system "clear"
-  puts square
-  puts "it's #{name_1}'s turn to play."
-  puts "Please select an available cell from the board!"
+system 'clear'
+puts square
+puts "it's #{name1}'s turn to play."
+puts 'Please select an available cell from the board!'
+number = gets.chomp
+ele1 = number.to_i
+until (1..9).any?(ele1)
+  puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele1)
   number = gets.chomp
   ele1 = number.to_i
-  until (1..9).any?(ele1)
-  puts "Invalid move. Please enter a number between 1-9" unless (1..9).any?(ele1)
-  number = gets.chomp
-  ele1 = number.to_i
-  end
-puts "It's a TIE" + "\n"
-puts "Game over"
+end
+puts "It's a TIE\n"
+puts 'Game over'
