8cf9c77e6deb0129d267c47968607709570aca87
Ángel Díaz <diazangel9311@gmail.com>
2021-05-11 10:22:45 -0500

    Rearrange until loop
    Add new methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,47 @@
 #!/usr/bin/env ruby
 square = "+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+"
+arr = []
+
+def input_names
+  flag = false
+  arr = []
+  name = gets.chomp
+  until flag
+    arr << name
+    if arr.all?(/^[a-zA-Z]+$/)
+      flag = true
+    else
+      puts 'Incorrect input, only letters accepted.'
+      arr.delete(name)
+      puts 'Type again your name:'
+      name = gets.chomp
+    end
+  end
+  name
+end
+
+def win_condition
+  system 'clear'
+  puts 'You win!'
+  gets
+end
+
+def move_input
+  number = gets.chomp
+  ele = number.to_i
+  until (1..9).any?(ele)
+    puts 'Invalid move.Please select a number between 1-9.'
+    number = gets.chomp
+    ele = number.to_i
+  end
+  ele
+end
+
 puts "Welcome to Tic Tac Toe Game. Let's begin\n"
 puts 'Enter the name of first player.'
-first_player = gets.chomp
+first_player = input_names
 puts 'Enter the name of second player.'
-second_player = gets.chomp
+second_player = input_names
 name1 = first_player[0].upcase + first_player[1..-1].downcase
 name2 = second_player[0].upcase + second_player[1..-1].downcase
 system 'clear'
@@ -12,105 +49,20 @@ puts "#{name1} will play with X, and #{name2} will play with O"
 puts "Let's start! Press Enter"
 gets
 system 'clear'
-# 2.times do
-#   system 'clear'
-#   puts square
-#   puts "it's #{name1}'s turn to play."
-#   puts 'Please select an available cell from the board!'
-#   number = gets.chomp
-#   ele = number.to_i
-#   until (1..9).any?(ele)
-#     puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele)
-#     number = gets.chomp
-#     ele = number.to_i
-#   end
-#   system 'clear'
-#   puts square
-#   puts "it's #{name2}'s turn to play."
-#   puts 'Please select an available cell from the board!'
-#   number = gets.chomp
-#   ele = number.to_i
-#   until (1..9).any?(ele)
-#     puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele)
-#     number = gets.chomp
-#     ele = number.to_i
-#   end
-# end
-# puts "#{name2} you WON the game!!!"
-# gets
-# system 'clear'
-# 2.times do
-#   system 'clear'
-#   puts square
-#   puts "it's #{name1}'s turn to play."
-#   puts 'Please select an available cell from the board!'
-#   number = gets.chomp
-#   ele1 = number.to_i
-#   until (1..9).any?(ele1)
-#     puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele1)
-#     number = gets.chomp
-#     ele1 = number.to_i
-#   end
 
-#   system 'clear'
-#   puts square
-#   puts "it's #{name2}'s turn to play."
-#   puts 'Please select an available cell from the board!'
-#   number = gets.chomp
-#   ele2 = number.to_i
-#   until (1..9).any?(ele2)
-#     puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele2)
-#     number = gets.chomp
-#     ele2 = number.to_i
-#   end
-# end
-
-# system 'clear'
-# puts square
-# puts "it's #{name1}'s turn to play."
-# puts 'Please select an available cell from the board!'
-# number = gets.chomp
-# ele1 = number.to_i
-# until (1..9).any?(ele1)
-#   puts 'Invalid move. Please enter a number between 1-9' unless (1..9).any?(ele1)
-#   number = gets.chomp
-#   ele1 = number.to_i
-# end
-# puts "It's a TIE\n"
-# puts 'Game over'
-
-def win_condition
-  if arr[0] == 1 && arr[2] == 3
-    true
-  else
-    false
-  end
-end
-def draw_condition
-  if arr.length == 5
-    true
-  else
-    false
-  end
-end
-while win_condition != true || draw_condition != true
+until arr.length > 9
   system 'clear'
   puts square
   puts "it's #{name1}'s turn to play."
   puts 'Please select an available cell from the board!'
-  def move_input
-    number = gets.chomp
-      ele = number.to_i
-      until (1..9).any?(ele)
-        puts "Invalid move.Please select a number between 1-9."
-        number = gets.chomp
-        ele = number.to_i
-      end
-  end
-  arr << ele
+  arr << move_input
+  break if arr.length == 9
+
+  system 'clear'
+  puts square
+  puts "it's #{name2}'s turn to play."
+  puts 'Please select an available cell from the board!'
+  arr << move_input
+  win_condition if arr.length == 4
 end
-  
-  
-  # You can write draw_condition here
-  name1 = name2
-  # assign name for the next turn 
+puts 'It a tie'
