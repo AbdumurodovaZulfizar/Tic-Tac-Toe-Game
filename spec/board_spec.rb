33286920303d0692e31cf0c34305fdfb4ef3a9ec
joshuaivie <joshua@lifeonline.com.ng>
2021-05-17 22:51:09 +0100

    add #insert test to board_spec.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,13 +4,20 @@ RSpec.describe Board do
   let(:board) { Board.new }
   describe '#initialize' do
     it 'creates sets variable' do
-      expect(board.sets).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]])
+      expect(board.sets).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9],
+                                 [3, 5, 7]])
     end
     it 'creates grid varable' do
       expect(board.grid).to eql("+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+")
     end
-    it 'cretes selections variable' do
+    it 'creates selections variable' do
       expect(board.selections).to eql([])
+    end
+  end
+
+  describe '#insert' do
+    it 'inserts number to the symbol' do
+      expect(board.insert(1, 'X')).to eql('X')
     end
   end
 end
